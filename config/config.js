14ed6eaf125e739614695843a3d759fadbbb7972
Malcolm Brito <malcolm.brito16@tectijuana.edu.mx>
2020-06-29 18:23:56 -0700

    ahorasimlab2

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
 
     port: process.env.PORT ||  3000,
-    db: process.env.MONGODB || 'mongodb://localhost:27017/shop',
+    db: process.env.MONGODB || 'mongodb+srv://admin1:admin1@contratos-database-a2ie7.mongodb.net/shop?retryWrites=true&w=majority',
     urlParser:{
         useNewUrlParser:true,
         useUnifiedTopology:true
