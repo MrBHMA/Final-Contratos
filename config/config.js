ea3b69609b16b2bfcad84501ec4de8d3fd1d2471
Malcolm Brito <malcolm.brito16@tectijuana.edu.mx>
2020-06-29 18:55:30 -0700

    funciona

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    port: process.env.PORT ||  3000,
+    port: process.env.PORT ||  5000,
     db: process.env.MONGODB || 'mongodb+srv://admin1:admin1@contratos-database-a2ie7.mongodb.net/shop?retryWrites=true&w=majority',
     urlParser:{
         useNewUrlParser:true,
